1adc60a7a22e46d46b22f4a3da96b6927234627f
AlanWeb93 <alan.zuritanuevo@gmail.com>
2021-01-05 08:39:37 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,18 @@
 
 process.env.PORT = process.env.PORT || 3000;
 
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// Base de datos
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'url de la base de datos en la nube (mongo atlas por ejemplo)';
+}
+
+process.env.URLDB = urlDB;
